7950c4eb7978e493073d411ccdd802e6fad281b9
SVS Sathvik <svssathvik@SVSs-MacBook-Air.local>
2023-10-14 15:40:33 +0530

    signup finish

--- server.js
+++ server.js
@@ -7,10 +7,52 @@ app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-const url = "mongodb+srv://uslsahithi8:sahi@2004@cluster0.3l5xyjq.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://uslsahithi8:sahi%402004@cluster0.3l5xyjq.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(url).then(()=>{
     console.log("Database connected!");
 });
+
+const UserSchema = new mongoose.Schema({
+    name : {
+        required : true,
+        type : String
+    },
+    email : {
+        required : true,
+        type: String,
+    },
+    password : {
+        required : true,
+        type:String
+    },
+    address : {
+        required : true,
+        type:String
+    },
+    contact:{
+        required : true,
+        type:String
+    },
+    admin : {
+        type : Boolean
+    }
+});
+const UserDB = new mongoose.model("users",UserSchema);
+
+app.post("/newUser",(req,res)=>{
+    const data = new UserDB({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+        address : req.body.address,
+        contact : req.body.contact,
+        admin : false
+    })
+    data.save();
+    res.redirect(req.get("referer"));
+})
+
+
 app.get("/",(req,res)=>{
     res.render("index");
 });
