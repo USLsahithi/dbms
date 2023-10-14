31647bc5a628e9677a2b4adb78ed8699443eacdb
SVS Sathvik <svssathvik@SVSs-MacBook-Air.local>
2023-10-14 14:49:28 +0530

    Ejs conig initialized

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(3500,()=>{
+    console.log("server running");
+});
