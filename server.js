026a54b762e836dadef71a0d5c9419ac823ea7d8
SVS Sathvik <svssathvik@SVSs-MacBook-Air.local>
2023-10-14 15:25:00 +0530

    mongodb

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
+const url = "mongodb+srv://uslsahithi8:sahi@2004@cluster0.3l5xyjq.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url).then(()=>{
+    console.log("Database connected!");
+});
 app.get("/",(req,res)=>{
     res.render("index");
 });
